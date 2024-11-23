83ab7bf7065be2a465c5c9feb434837ab9d49300
MauricioJeria <mauricio.tst15@gmail.com>
2024-11-23 18:15:17 -0300

    act apk v.1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.MesaAyuda.app',
+  appName: 'MesaAyuda',
+  webDir: 'www'
+};
+
+export default config;
